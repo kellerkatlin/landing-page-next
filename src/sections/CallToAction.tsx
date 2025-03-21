56102cab885111f14a072751e1277c06175e74ee
kellerkatlin <kellerkatlin.k@gmail.com>
2025-03-20 22:30:34 -0500

    call to action animation

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,11 +1,23 @@
+"use client";
 import ArrowRight from "@/assets/arrow-right.svg";
 import starImage from "@/assets/star.png";
 import springImage from "@/assets/spring.png";
-import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export default function CallToAction() {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
   return (
-    <section className="bg-gradient-to-b from-white to-[#D2DCFF] pb-24 pt-40 overflow-x-clip">
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-white to-[#D2DCFF] pb-24 pt-40 overflow-x-clip"
+    >
       <div className="container">
         <div className="section-heading relative">
           <h2 className="section-title">Regístrate gratis hoy</h2>
@@ -13,16 +25,18 @@ export default function CallToAction() {
             Celebre la alegría de sus logros con una aplicación diseñada para
             seguir su progreso y motivar sus esfuerzos.
           </p>
-          <Image
-            src={starImage}
+          <motion.img
+            src={starImage.src}
             alt="Star"
             width={360}
+            style={{ translateY }}
             className="absolute -left-[350px] -top-[137px]"
           />
-          <Image
-            src={springImage}
+          <motion.img
+            src={springImage.src}
             alt="Spring"
             width={360}
+            style={{ translateY }}
             className="absolute -right-[331px] -top-[19px]"
           />
         </div>
